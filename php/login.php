745efdc0ab513bbb853d0b2411a26944942ae0da
AnKo <anko96x@gmail.com>
2018-05-22 16:04:27 +0300

    All

---
+++ php/login.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "0";
+//
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+//
+// require_once('dbConnect.php');
+//
+// // An insertion query. $result will be `true` if successful
+// $result = db_query("SELECT * FROM 'clients' WHERE username=='$username' AND password=='$password'";
+//
+// $row = mysql_fetch_array($result);
+//
+// if(empty($row))
+// {
+// 	print(0);
+// }
+//
+// else
+// {
+// 	echo $row[0];
+// 	echo $row[3];
+// 	echo $row[4];
+// 	echo $row[5];
+// }
+
+?>
