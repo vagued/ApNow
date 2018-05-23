7859f3277829d75c7becbca4ee3a5df1c47f673b
AnKo <anko96x@gmail.com>
2018-05-23 20:07:59 +0300

    asd

--- php/login.php
+++ php/login.php
@@ -1,37 +1,44 @@
 <?php
 
-// $username = $_POST['username'];
-// $password = $_POST['password'];
+if (isset($_POST))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+}
+else {
+	echo "No POST made";
+}
 
-$username = "asd";
-$password = "asd";
+// $username = "asd";
+// $password = "asd";
 
 require_once('dbConnect.php');
 
 // An insertion query. $result will be `true` if successful
-$result = db_query("SELECT * FROM 'clients' WHERE username=='$username' AND password=='$password'");
+$result = db_query("SELECT * FROM clients WHERE username='$username' AND password='$password'");
 
 $row = mysqli_fetch_array($result);
 
 if(empty($row))
 {
-	// echo '<script>';
-	// echo 'selectElementById("msg").innerHTML = "Invalid credidentials";';
-	// echo '</script>';
-	echo "nah";
+	echo "Invalid credidentials, please try again.";
+	echo "\nReloading...";
+	sleep(2);
+	echo '<script>';
+	echo 'window.location.href = "../html/login.html";';
+	echo '</script>';
 }
 
 else
 {
-
-	// echo '<script>';
-	// echo 'var sessionStorage.idClient = ' . json_encode($row[0]) . ';';
-	// echo 'var sessionStorage.firstName = ' . json_encode($row[3]) . ';';
-	// echo 'var sessionStorage.lastName = ' . json_encode($row[4]) . ';';
-	// echo 'var sessionStorage.email = ' . json_encode($row[5]) . ';';
-	// echo '</script>';
-
-	echo "works";
+	echo '<!DOCTYPE html><html><body><script>';
+	echo 'var sessionStorage.idClient = ' . json_encode($row[0]) . ';';
+	echo 'var sessionStorage.firstName = ' . json_encode($row[3]) . ';';
+	echo 'var sessionStorage.lastName = ' . json_encode($row[4]) . ';';
+	echo 'var sessionStorage.email = ' . json_encode($row[5]) . ';';
+	echo 'window.location.href = "../html/homepage.html";';
+	echo '</script></body></html>';
+	// echo "works";
 }
 
 ?>
