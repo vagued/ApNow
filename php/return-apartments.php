670f131d7151af6c6458c66dcd3628357c3a2885
AnKo <anko96x@gmail.com>
2018-06-16 17:41:09 +0300

    asd

--- php/return-apartments.php
+++ php/return-apartments.php
@@ -8,12 +8,13 @@ $rows = mysqli_num_rows($result);
 $myArray = array();
 
 if($rows==0)
-	echo 'No apartments for the selected location.';
+	echo 'There was an error';
 else
 {
-	while($row = mysqli_fetch_array($result)) {
+	while($row = mysqli_fetch_array($result))
+	{
   	$myArray[] = $row;
-  }
+	}
 	echo json_encode($myArray);
 
 	// $data=array(1,"asd");
