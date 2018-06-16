5ce9214f97513cc4f44fe1bc022edad759637397
AnKo <anko96x@gmail.com>
2018-06-16 13:12:11 +0300

    asd

--- php/return-apartments.php
+++ php/return-apartments.php
@@ -5,31 +5,25 @@ require_once('dbConnect.php');
 $result = db_query("SELECT idapartment, title FROM apartments");
 
 $rows = mysqli_num_rows($result);
+$myArray = array();
 
 if($rows==0)
 	echo 'No apartments for the selected location.';
 else
 {
-	$data=array(1,"asd");
-  while($row = mysqli_fetch_array($result))
-  {
-     array_merge(data, array(
-      "idapartment" => $row['idapartment'],
-      "title" => $row['title']
-    ));
+	while($row = mysqli_fetch_array($result)) {
+  	$myArray[] = $row;
   }
-	echo json_encode(data);
+	echo json_encode($myArray);
+
+	// $data=array(1,"asd");
+  // while($row = mysqli_fetch_array($result))
+  // {
+  //    array_merge(data, array(
+  //     "idapartment" => $row['idapartment'],
+  //     "title" => $row['title']
+  //   ));
+  // }
+	// echo json_encode(data);
 }
-
-// var data = {};
-// if (responseText.indexOf("NOT A VALID") == -1) {
-//     data = JSON.parse(responseText);
-// }
-// // Check the values or just place an empty string if undefined
-// document.getElementById("whse" + userNumber).innerHTML = data.warehouse || '';
-// document.getElementById("txtHint" + userNumber).innerHTML = data.description || '';
-// document.getElementById("su" + userNumber).innerHTML = data.sellingunits || '';
-// document.getElementById("suqty" + userNumber).innerHTML = data.suqty || '';
-// document.getElementById("category" + userNumber).innerHTML = data.category || '';
-
 ?>
