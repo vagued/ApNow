e89a9151ad905446460aabfbecf617f6fc56984a
AnKo <anko96x@gmail.com>
2018-06-21 21:47:41 +0300

    asd

--- php/submit-renting.php
+++ php/submit-renting.php
@@ -2,12 +2,55 @@
 
 require_once('dbConnect.php');
 
+$idclient = $_REQUEST["idclient"];
 $idapartment = $_REQUEST["idapartment"];
+$checkin = $_REQUEST["checkin"];
+$checkout = $_REQUEST["checkout"];
 
-$result = db_query("INSERT INTO clients (username, password, firstname, lastname, email)
-  VALUES ('$username','$password','$firstname','$lastname','$email')");
+$result1 = db_query("
+  SELECT apartments.checkin <= '$checkin' AND apartments.checkout >= '$checkout " .
+  "FROM apartments " .
+  "WHERE apartments.idapartment=$idapartment");
 
-if($result === false)
-    echo "Failed to insert\n";
+$result2 = db_query("
+  SELECT IF" .
+  "(" .
+    "(SELECT count(*) FROM rentings " .
+    "WHERE rentings.idapartment=$idapartment" .
+    "AND rentings.checkin >= '$checkin'" .
+    "AND rentings.checkout <= '$checkout'" .
+  ")=0," .
+  "'1', '0')"
+);
+
+if($result1 === false)
+    echo "Failed to insert #1\n";
+
+if($result2 === false)
+    echo "Failed to insert #2\n";
+
+mysqli_data_seek($result1,1);
+$row=mysqli_fetch_row($result1);
+$r1=row[0];
+
+mysqli_data_seek($result2,1);
+$row=mysqli_fetch_row($result2);
+$r2=row[0];
+
+if($r1&&$r2)
+{
+  echo 1;
+
+  $result3 = db_query("" .
+    "INSERT INTO apartments (idclient, title, location, description, checkin, checkout, extension)
+      VALUES ('$idclient','$title','$location','$description','$checkin','$checkout','$extension')");
+
+  if($result3 === false)
+      echo "Failed to insert #3\n";
+}
+else
+{
+  echo 0;
+}
 
 ?>
