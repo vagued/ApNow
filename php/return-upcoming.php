9e36fba67f853d48525d0231243a3d976d7e5a08
AnKo <anko96x@gmail.com>
2018-06-24 15:43:41 +0300

    asd

--- php/return-upcoming.php
+++ php/return-upcoming.php
@@ -4,16 +4,17 @@ require_once('dbConnect.php');
 
 $idclient = $_REQUEST["q"];
 
-$result = db_query("SELECT title, checkin, checkout FROM apartments, rentings
-	WHERE rentings.idclient='$idclient'
-		AND rentings.idapartment=apartment.idapartment
+$result = db_query("SELECT title, rentings.checkin, rentings.checkout
+	FROM apartments, rentings
+	WHERE rentings.idclient=$idclient
+		AND rentings.idapartment=apartments.idapartment
 		AND apartments.checkin>='" . date("Y/m/d") . "'");
 
 $rows = mysqli_num_rows($result);
 $myArray = array();
 
 if($rows==0)
-	echo 'There was an error';
+	echo 0;
 else
 {
 	while($row = mysqli_fetch_array($result))
